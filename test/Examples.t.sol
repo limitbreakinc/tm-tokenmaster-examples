99955aa5ee1d74fab43e6c4bb4c10e82c109f579
Thomas Parent <thomas.parent@limitbreak.com>
2025-01-03 15:27:55 -0600

    start of examples repo

---
+++ test/Examples.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import "@limitbreak/tokenmaster/test/TokenMaster.t.sol";
+
+contract ExamplesTest is TokenMasterTest {
+
+    function testExampleStandardPool() public {
+        
+    }   
+}
